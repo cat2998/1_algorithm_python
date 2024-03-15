db13a25ea373116e6ac169791e22e623d8157038
jihyun <cat2998@naver.com>
2024-03-15 17:52:23 +0900

    jihyun 2628 update

--- 24_2628.py
+++ 24_2628.py
@@ -1,25 +1,19 @@
 m, n = map(int, input().split())
 t = int(input())
-m_list = list(True for i in range(m + 1))
-n_list = list(True for i in range(n + 1))
+m_list = [0, m]
+n_list = [0, n]
+max_m = 0
+max_n = 0
 for i in range(t):
     a, b = map(int, input().split())
     if a == 0:
-        n_list[b] = False
+        n_list.append(b)
     else:
-        m_list[b] = False
-max_m = 0
-max_n = 0
-cnt = 0
-for i in range(1, len(m_list)):
-    cnt += 1
-    if m_list[i] == False or i == len(m_list) - 1:
-        max_m = max(max_m, cnt)
-        cnt = 0
-cnt = 0
-for i in range(1, len(n_list)):
-    cnt += 1
-    if n_list[i] == False or i == len(n_list) - 1:
-        max_n = max(max_n, cnt)
-        cnt = 0
+        m_list.append(b)
+m_list.sort()
+n_list.sort()
+for i in range(len(m_list) - 1):
+    max_m = max(max_m, m_list[i + 1] - m_list[i])
+for i in range(len(n_list) - 1):
+    max_n = max(max_n, n_list[i + 1] - n_list[i])
 print(max_m * max_n)
