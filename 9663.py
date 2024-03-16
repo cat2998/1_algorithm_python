47a610cfe5e3d25d2a8a4f20ec576b965e655c0b
hkyoo <hkyoo988@naver.com>
2024-03-16 20:32:59 +0900

    recursion

--- 9663.py
+++ 9663.py
@@ -14,6 +14,9 @@ def set(i):
             if i == n-1:
                 cnt += 1
             else:
+                # 격자점을 행을 x좌표로 열을 y좌표로 한 다음 오른쪽대각선은 x+y,
+                # 왼쪽 대각선은 y-x인데 -7~7까지의 값이 나오기 때문에 공통의 격자점에서
+                # 대각선의 값이 동일한 값이 나오도록 하기 위해서 +7을 해주는 것.
                 flag_a[j] = flag_b[i+j] = flag_c[i-j+n-1] = True
                 set(i+1)
                 flag_a[j] = flag_b[i+j] = flag_c[i-j+n-1] = False
